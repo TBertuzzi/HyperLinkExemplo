2b9cc03dcd2844b4c8f20f37048f6513531c91ef
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2019-06-24 13:45:21 -0300

    :tada: Subindo exemplo no github

---
+++ HyperLinkExemplo/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace HyperLinkExemplo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public ICommand TapCommand => new Command<string>(AbrirNavegador);
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        void AbrirNavegador(string url)
+        {
+            Device.OpenUri(new Uri(url));
+        }
+    }
+}
